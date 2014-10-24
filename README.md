7d1701f15508d1595c9f50257965f43bd261956c
mudler <mudler@dark-lab.net>
2014-10-24 22:51:11 +0200

    first release

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Minosse - It's new $module
+
+# SYNOPSIS
+
+    use Minosse;
+
+# DESCRIPTION
+
+Minosse is ...
+
+# LICENSE
+
+Copyright (C) mudler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mudler <mudler@dark-lab.net>
---
+++ lib/Minosse.pm
@@ -0,0 +1,39 @@
+package Minosse;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Minosse - It's new $module
+
+=head1 SYNOPSIS
+
+    use Minosse;
+
+=head1 DESCRIPTION
+
+Minosse is ...
+
+=head1 LICENSE
+
+Copyright (C) mudler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mudler E<lt>mudler@dark-lab.netE<gt>
+
+=cut
+
